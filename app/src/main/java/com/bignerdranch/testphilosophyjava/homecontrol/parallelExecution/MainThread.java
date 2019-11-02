3989babe90955ed2f1cf314d19cdfa54a10c8feb
Leonid <bankmtk@gmail.com>
2019-11-02 21:43:23 +0300

    Add class MainThread

---
+++ app/src/main/java/com/bignerdranch/testphilosophyjava/homecontrol/parallelExecution/MainThread.java
@@ -0,0 +1,8 @@
+package com.bignerdranch.testphilosophyjava.homecontrol.parallelExecution;
+
+public class MainThread {
+    public static void main(String[] args) {
+        LiftOff launch = new LiftOff();
+        launch.run();
+    }
+}
