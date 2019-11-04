0dfd5d74be82aa74db44d46e3fa37c5c6cace2bb
Leonid <bankmtk@gmail.com>
2019-11-04 20:19:35 +0300

    Synchronized (page 925)

---
+++ app/src/main/java/com/bignerdranch/testphilosophyjava/homecontrol/parallelExecution/SynchronizedEvenGenerator.java
@@ -0,0 +1,15 @@
+package com.bignerdranch.testphilosophyjava.homecontrol.parallelExecution;
+
+public class SynchronizedEvenGenerator extends IntGenerator {
+    private int currentEvenValue = 0;
+    public synchronized int next(){
+        ++currentEvenValue;
+        Thread.yield();
+        ++currentEvenValue;
+        return currentEvenValue;
+    }
+
+    public static void main(String[] args) {
+        EvenChecker.test(new SynchronizedEvenGenerator());
+    }
+}
