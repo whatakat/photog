f99bde52e7eadee6b1850b2b0aec895dbdf2931f
Leonid <bankmtk@gmail.com>
2019-11-05 19:09:30 +0300

    SerialNumberGenerator

---
+++ app/src/main/java/com/bignerdranch/testphilosophyjava/homecontrol/parallelExecution/SerialNumberGenerator.java
@@ -0,0 +1,8 @@
+package com.bignerdranch.testphilosophyjava.homecontrol.parallelExecution;
+
+public class SerialNumberGenerator {
+    private static volatile int serialNumber = 0;
+    public static int nextSerialNumber(){
+        return serialNumber++;
+    }
+}
