09806375e6262f769ce43c2cf895fac5f5dee649
Leonid <bankmtk@gmail.com>
2019-11-05 21:39:10 +0300

    AtomicGenerator

---
+++ app/src/main/java/com/bignerdranch/testphilosophyjava/homecontrol/parallelExecution/atomicClass/AtomicEvenGenerator.java
@@ -0,0 +1,18 @@
+package com.bignerdranch.testphilosophyjava.homecontrol.parallelExecution.atomicClass;
+
+import com.bignerdranch.testphilosophyjava.homecontrol.parallelExecution.EvenChecker;
+import com.bignerdranch.testphilosophyjava.homecontrol.parallelExecution.IntGenerator;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicEvenGenerator extends IntGenerator {
+    private AtomicInteger currentEvenValue =
+            new AtomicInteger(0);
+    public int next(){
+        return currentEvenValue.addAndGet(2);
+    }
+
+    public static void main(String[] args) {
+        EvenChecker.test(new AtomicEvenGenerator());
+    }
+}
