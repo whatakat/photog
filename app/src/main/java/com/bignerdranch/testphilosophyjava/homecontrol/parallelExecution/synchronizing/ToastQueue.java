bb7243449b773b0bb67e6e7492236065c2a4d222
Leonid <bankmtk@gmail.com>
2019-11-07 23:52:18 +0300

    add class ToastQueue

---
+++ app/src/main/java/com/bignerdranch/testphilosophyjava/homecontrol/parallelExecution/synchronizing/ToastQueue.java
@@ -0,0 +1,6 @@
+package com.bignerdranch.testphilosophyjava.homecontrol.parallelExecution.synchronizing;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class ToastQueue extends LinkedBlockingQueue<Toast> {
+}
