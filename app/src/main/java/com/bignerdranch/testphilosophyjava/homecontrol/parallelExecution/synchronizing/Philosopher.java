4ad56f96c0b125a40b6ac127639e810d9663306f
Leonid <bankmtk@gmail.com>
2019-11-08 19:38:59 +0300

    add Philosopher (page 979)

---
+++ app/src/main/java/com/bignerdranch/testphilosophyjava/homecontrol/parallelExecution/synchronizing/Philosopher.java
@@ -0,0 +1,44 @@
+package com.bignerdranch.testphilosophyjava.homecontrol.parallelExecution.synchronizing;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import static com.bignerdranch.testphilosophyjava.homecontrol.print.Print.print;
+
+public class Philosopher implements Runnable {
+    private Chopstick left;
+    private Chopstick right;
+    private final int id;
+    private final int ponderFactor;
+    private Random random = new Random(47);
+    private void pause() throws InterruptedException{
+        if (ponderFactor ==0)return;
+        TimeUnit.MILLISECONDS.sleep(
+                random.nextInt(ponderFactor*250));
+    }
+    public Philosopher(Chopstick left, Chopstick right,int ident, int ponder){
+        this.left = left;
+        this.right = right;
+        id = ident;
+        ponderFactor = ponder;
+    }
+    public void run(){
+        try {
+            while (!Thread.interrupted()){
+                print(this+" "+"thinking");
+                pause();
+                print(this+" "+"grabbing right");
+                right.take();
+                print(this+" "+"grabbing left");
+                left.take();
+                print(this+" "+"eating");
+                pause();
+                right.drop();
+                left.drop();
+            }
+        }catch (InterruptedException e){
+            print(this+" "+"exiting via interrupt");
+        }
+    }
+    public String toString(){return "Philosopher "+id;}
+}
