7b17793123f9f042ebc1c89068496e88749f9df8
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-11-20 22:29:53 +0300

    added constant for storing the result

--- app/src/main/java/com/bignerdranch/photogallery/QueryPreferences.java
+++ app/src/main/java/com/bignerdranch/photogallery/QueryPreferences.java
@@ -4,7 +4,8 @@ import android.content.Context;
 import android.preference.PreferenceManager;
 
 public class QueryPreferences {
-    public static final String PREF_SEARCH_QUERY = "searchQuery";
+    private static final String PREF_SEARCH_QUERY = "searchQuery";
+    private static final String PREF_LAST_RESULT_ID ="lastResultId";
     public static String getStoredQuery(Context context){
         return PreferenceManager.getDefaultSharedPreferences(context)
                 .getString(PREF_SEARCH_QUERY, null);
@@ -13,6 +14,16 @@ public class QueryPreferences {
         PreferenceManager.getDefaultSharedPreferences(context)
                 .edit()
                 .putString(PREF_SEARCH_QUERY, query)
+                .apply();
+    }
+    public static String getLastResultId(Context context){
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(PREF_LAST_RESULT_ID, null);
+    }
+    public static void setLastResultId(Context context, String lastResultId){
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putString(PREF_LAST_RESULT_ID, lastResultId)
                 .apply();
     }
 }
