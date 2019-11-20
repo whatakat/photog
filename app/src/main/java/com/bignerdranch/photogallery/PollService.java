400892889d74499046540928205f8b50ba7476b4
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-11-20 23:49:50 +0300

    added signal method in PollService

--- app/src/main/java/com/bignerdranch/photogallery/PollService.java
+++ app/src/main/java/com/bignerdranch/photogallery/PollService.java
@@ -1,18 +1,37 @@
 package com.bignerdranch.photogallery;
 
+import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
+import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class PollService extends IntentService {
     private static final String TAG = "PollService";
+    private static final long POLL_INTERNAL_MS = TimeUnit.MINUTES.toMillis(1);
+
     public static Intent newIntent(Context context){
         return new Intent(context, PollService.class);
+    }
+    public static void setServiceAlarm(Context context, boolean isOn){
+        Intent i = PollService.newIntent(context);
+        PendingIntent pi = PendingIntent.getService(context, 0, i, 0);
+        AlarmManager alarmManager = (AlarmManager)
+                context.getSystemService(Context.ALARM_SERVICE);
+        if (isOn){
+            alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME,
+                    SystemClock.elapsedRealtime(), POLL_INTERNAL_MS, pi);
+        }else {
+            alarmManager.cancel(pi);
+            pi.cancel();
+        }
     }
     public PollService(){
         super(TAG);
