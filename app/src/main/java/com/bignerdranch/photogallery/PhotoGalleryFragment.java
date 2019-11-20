b403b6af40680a69dac1060e8653dc865955fe8b
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-11-20 22:02:35 +0300

    startService

--- app/src/main/java/com/bignerdranch/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/bignerdranch/photogallery/PhotoGalleryFragment.java
@@ -1,5 +1,6 @@
 package com.bignerdranch.photogallery;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
@@ -43,6 +44,8 @@ public class PhotoGalleryFragment extends Fragment {
         setRetainInstance(true);
         setHasOptionsMenu(true);
         updateItems();
+        Intent i = PollService.newIntent(getActivity());
+        getActivity().startService(i);
         Handler responseHandler = new Handler();
 
 
