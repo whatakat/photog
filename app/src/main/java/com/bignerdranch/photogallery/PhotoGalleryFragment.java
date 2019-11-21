205c4dcee8a5ea22183dff6bd5eba8d38bb64f0f
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-11-21 22:58:49 +0300

    add searchView.setOnSearchClickListener

--- app/src/main/java/com/bignerdranch/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/bignerdranch/photogallery/PhotoGalleryFragment.java
@@ -102,6 +102,13 @@ public class PhotoGalleryFragment extends Fragment {
                 return false;
             }
         });
+        searchView.setOnSearchClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String query = QueryPreferences.getStoredQuery(getActivity());
+                searchView.setQuery(query, false);
+            }
+        });
     }
 
     @Override
