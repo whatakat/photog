53246f71411594b664e88d683ea92400cb8ca383
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-11-24 00:40:32 +0300

    using WebChromeClient

--- app/src/main/java/com/bignerdranch/photogallery/PhotoPageFragment.java
+++ app/src/main/java/com/bignerdranch/photogallery/PhotoPageFragment.java
@@ -4,16 +4,20 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class PhotoPageFragment extends VisibleFragment {
     public static final String ARG_URI = "photo_page_url";
     private Uri mUri;
     private WebView mWebView;
+    private ProgressBar mProgressBar;
     public static PhotoPageFragment newInstance(Uri uri){
         Bundle args = new Bundle();
         args.putParcelable(ARG_URI, uri);
@@ -33,8 +37,24 @@ public class PhotoPageFragment extends VisibleFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_photo_page, container, false);
+        mProgressBar=(ProgressBar)v.findViewById(R.id.progress_bar);
+        mProgressBar.setMax(100);
         mWebView = (WebView)v.findViewById(R.id.web_view);
         mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebChromeClient(new WebChromeClient(){
+            public void onProgressChange(WebView webView, int newProgress){
+                if (newProgress==100){
+                    mProgressBar.setVisibility(View.GONE);
+                }else {
+                    mProgressBar.setVisibility(View.VISIBLE);
+                    mProgressBar.setProgress(newProgress);
+                }
+            }
+            public void onReceivedTitle(WebView webView, String title){
+                AppCompatActivity activity = (AppCompatActivity) getActivity();
+                activity.getSupportActionBar().setSubtitle(title);
+            }
+        });
         mWebView.setWebViewClient(new WebViewClient());
         mWebView.loadUrl(mUri.toString());
         return v;
