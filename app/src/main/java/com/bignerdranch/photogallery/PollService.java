495b91a781da3c0f4e9b8b7f1c818ac7229fd538
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-11-23 14:38:18 +0300

    sending ordered broadcast intents

--- app/src/main/java/com/bignerdranch/photogallery/PollService.java
+++ app/src/main/java/com/bignerdranch/photogallery/PollService.java
@@ -1,5 +1,6 @@
 package com.bignerdranch.photogallery;
 
+import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.IntentService;
 import android.app.Notification;
@@ -24,6 +25,8 @@ public class PollService extends IntentService {
             "com.bignerdranch.photogallery.SHOW_NOTIFICATION";
     public static final String PERM_PRIVATE =
             "com.bignerdranch.photogallery.PRIVATE";
+    public static final String REQUEST_CODE = "REQUEST_CODE";
+    public static final String NOTIFICATION = "NOTIFICATION";
 
     public static Intent newIntent(Context context){
         return new Intent(context, PollService.class);
@@ -84,12 +87,16 @@ public class PollService extends IntentService {
                     .setContentIntent(pi)
                     .setAutoCancel(true)
                     .build();
-            NotificationManagerCompat notificationManager =
-                    NotificationManagerCompat.from(this);
-            notificationManager.notify(0,notification);
-            sendBroadcast(new Intent(ACTION_SHOW_NOTIFICATION));
         }
         QueryPreferences.setLastResultId(this, resultId);
+    }
+    private void showBackgroundNotification(int requestCode, Notification notification)
+    {
+        Intent i = Intent(ACTION_SHOW_NOTIFICATION);
+        i.putExtra(REQUEST_CODE, requestCode);
+        i.putExtra(NOTIFICATION, notification);
+        sendOrderedBroadcast(i,PERM_PRIVATE, null, null,
+                Activity.RESULT_OK, null, null);
     }
     private boolean isNetworkAvailableAndConnected(){
         ConnectivityManager cm =
