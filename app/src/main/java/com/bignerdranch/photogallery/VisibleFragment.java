400e6cd7f66a67daa29d17cffda5e04a93d48e11
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-11-23 14:26:47 +0300

    return result VisibleFragment

--- app/src/main/java/com/bignerdranch/photogallery/VisibleFragment.java
+++ app/src/main/java/com/bignerdranch/photogallery/VisibleFragment.java
@@ -1,10 +1,12 @@
 package com.bignerdranch.photogallery;
 
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.widget.Toast;
 
 public abstract class VisibleFragment extends Fragment {
@@ -26,10 +28,8 @@ public abstract class VisibleFragment extends Fragment {
     private BroadcastReceiver mOnShowNotification = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            Toast.makeText(getActivity(),
-                    "Got a broadcast:"+intent.getAction(),
-                    Toast.LENGTH_LONG)
-                    .show();
+            Log.i(TAG, "canceling notification");
+            setResultCode(Activity.RESULT_CANCELED);
         }
     };
 }
